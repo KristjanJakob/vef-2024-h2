86da78aa1daa4bbb42838c4d022a6197d45fad64
Kristján Jakob Ásgrímsson <kristjanjakobasgrimsson@rfc1918.rhi.hi.is>
2024-11-20 13:25:53 +0000

    Uppfæra allar js files

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-import { fetcher } from './lib/fetcher.js';
-import { renderContentPage } from './lib/pages/content-page.js';
 import { renderIndexPage } from './lib/pages/index-page.js';
 import { renderSubpage } from './lib/pages/sub-page.js';
+import { renderContentPage } from './lib/pages/content-page.js';
+import { fetcher } from './lib/fetcher.js';
 
 async function render(root, querystring) {
   const mainIndexJson = await fetcher('data/index.json');
@@ -10,19 +10,21 @@ async function render(root, querystring) {
   const type = params.get('type');
   const content = params.get('content');
 
-  console.log(type, content);
-
   if (!type) {
     return renderIndexPage(root, mainIndexJson);
   }
 
   if (content) {
-    return renderContentPage(root, mainIndexJson);
+    const contentJsonFile = `data/${type}/${content}.json`;
+    const contentJson = await fetcher(contentJsonFile);
+    return renderContentPage(root, mainIndexJson, contentJson);
   }
 
   renderSubpage(root, mainIndexJson, type);
 }
 
 const root = document.querySelector('#app');
+window.addEventListener('popstate', () => render(root, window.location.search));
 
 render(root, window.location.search);
+
